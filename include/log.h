52f3edbfcec37d5af761fc564306ee115794c844
Kevin Miller <kevinm@goat.enterprises>
2024-06-22 17:50:59 -0700

    Remove COLON lexer type

--- include/log.h
+++ include/log.h
@@ -20,21 +20,30 @@
 #endif
 
 // Helper macro to support variable arguments
-#define LOG_FORMAT(level, tag, fmt, ...) \
+#define LOG_FORMAT(level, fmt, ...) \
     do { \
         if (level <= DEBUG_LEVEL) { \
+            if (level == DEBUG_LEVEL_ERROR) { \
+                std::cerr << "\033[1;31m"; \
+            } else if (level == DEBUG_LEVEL_WARN) { \
+                std::cerr << "\033[1;33m"; \
+            } else if (level == DEBUG_LEVEL_INFO) { \
+                std::cerr << "\033[1;32m"; \
+            } else if (level == DEBUG_LEVEL_DEBUG) { \
+                std::cerr << "\033[1;34m"; \
+            } else if (level == DEBUG_LEVEL_TRACE) { \
+                std::cerr << "\033[1;37m"; \
+            } \
             std::ostringstream os; \
             os << __FILE__ << ":" << __LINE__; \
-            if (level == DEBUG_LEVEL_DEBUG || level == DEBUG_LEVEL_TRACE) { \
-                os << " (" << __FUNCTION__ << ")"; \
-            } \
-            os << " [" << tag << "] " << fmt << format_log(__VA_ARGS__) << std::endl; \
+            os << " " << fmt << format_log(__VA_ARGS__) << std::endl; \
             std::cerr << os.str(); \
+            std::cerr << "\033[0m"; \
         } \
     } while(0)
 
 // Fallback when no arguments are provided
-#define LOG(level, fmt, ...) LOG_FORMAT(level, "", fmt, ##__VA_ARGS__)
+#define LOG(level, fmt, ...) LOG_FORMAT(level, fmt, ##__VA_ARGS__)
 
 // Function to format variable arguments
 template<typename... Args>
